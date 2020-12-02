03c7160c1b05f7c9e5d9fb665fa4d0dcd8d6992f
Ryan Maunu <rmaunu@wayfair.com>
2020-12-02 15:55:45 -0500

    solution to day 1

--- advent_of_code_2020/advent_of_code.py
+++ advent_of_code_2020/advent_of_code.py
@@ -1,6 +1,6 @@
 import logging
 
-from .utils import module_fuel
+from .utils import find_pair_match_2020, find_triplet_match_2020
 
 
 logger = logging.getLogger(__name__)
@@ -14,13 +14,22 @@ class AdventOfCode(object):
     def run(self, task, input_file):
         method_to_call = getattr(AdventOfCode, f"command_{task}")
         method_to_call(input_file)
+        return
+
+    @staticmethod
+    def read_file(input_file):
+        with open(input_file, "r") as f:
+            lines = f.readlines()
+        return lines
 
     @staticmethod
     def command_1(input_file):
-        masses = []
-        with open(input_file, "r") as f:
-            lines = f.readlines()
-            masses = [int(mass) for mass in lines]
-        print(masses)
-        fuel = sum([module_fuel(mass) for mass in masses])
-        logger.info(fuel)
+        expenses = [int(expense) for expense in AdventOfCode.read_file(input_file)]
+        match_pair = find_pair_match_2020(expenses)
+        match_pair_multiple = match_pair[0] * match_pair[1]
+        logger.info(f"The matching pair is {match_pair}, which is {match_pair_multiple} when multiplied")
+
+        match_triplet = find_triplet_match_2020(expenses)
+        match_triple_multiple = match_triplet[0] * match_triplet[1] * match_triplet[2]
+        logger.info(f"The matching triplet is {match_triplet}, which is {match_triple_multiple} when multiplied")
+
--- advent_of_code_2020/utils/utils.py
+++ advent_of_code_2020/utils/utils.py
@@ -1,9 +1,50 @@
-def module_fuel(mass):
-    if mass > 0:
-        fuel = mass // 3  - 2
-        if fuel > 0:
-            return fuel + module_fuel(fuel)
-        else:
-            return 0
-    else:
-        return 0
+import numpy as np
+
+
+# The main function to sort an array of given size
+def sort_list(arr, kind='quicksort'):
+    return np.sort(arr, kind=kind).tolist()
+
+def find_pair_match(arr, target):
+    arr = sort_list(arr)
+    num_elements = len(arr)
+
+    for num in arr[:(num_elements // 2)]:
+        match_target = target - num
+        for other_num in arr[::-1]:
+            if other_num < match_target:
+                break
+            elif other_num == match_target:
+                return (num, other_num)
+
+    return (None, None)
+
+
+def find_triplet_match(arr, target):
+    arr = sort_list(arr)
+    num_elements = len(arr)
+
+    for i, first_num in enumerate(arr):
+        top_index = num_elements
+        for second_num in arr[(i + 1):]:
+            match_target = target - first_num - second_num
+
+            if match_target < 0:
+                break
+
+            for last_num in arr[top_index::-1]:
+                if last_num < match_target:
+                    break
+                elif last_num == match_target:
+                    return (first_num, second_num, last_num)
+                top_index -= 1
+
+    return (None, None, None)
+
+
+def find_pair_match_2020(arr):
+    return find_pair_match(arr, 2020)
+
+
+def find_triplet_match_2020(arr):
+    return find_triplet_match(arr, 2020)
